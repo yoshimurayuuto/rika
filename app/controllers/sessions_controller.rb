b80ec23f452f83739a15a34af0e33635cfbdd0d7
Yuuto Yoshimura <0615yutoyoshimu@gmail.com>
2019-10-15 10:43:03 +0900

    一時セッションログイン実装

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,6 +5,8 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       # ログイン成功した場合
+      session[:user_id] = user.id
+      redirect_to user_path(user.id)
     else
       flash.now[:danger] = 'ログインに失敗しました'
       render 'new'
