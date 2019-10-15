7b501aa26a6ac16a105dce2d4bf2769c712cfd2a
Yuuto Yoshimura <0615yutoyoshimu@gmail.com>
2019-10-15 11:41:30 +0900

    辞書機能作成

--- app/mailers/contact_mailer.rb
+++
@@ -1,7 +0,0 @@
-class ContactMailer < ApplicationMailer
-  def contact_mail(contact)
-    @contact = contact
-
-    mail to: "0615yutoyoshimu@gmail.com", subject: "お問い合わせの確認メール"
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  resources :contacts
   resources :sessions, only: [:new, :create, :destroy]
   resources :users
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
