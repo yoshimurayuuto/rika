8fdd88cc80c24d07e2ac34fc2c08446eb7f4bfa4
Yuuto Yoshimura <0615yutoyoshimu@gmail.com>
2019-11-14 10:32:20 +0900

    修正root

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 Rails.application.routes.draw do
+  root to: 'users#new'
   resources :feeds do
     collection do
       post :confirm
     end
   end
   resources :sessions, only: [:new, :create, :destroy]
-  resources :users, only: [:create, :destroy, :index, :show, :edit]
+  resources :users
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   resources :favorites, only: [:create, :destroy, :index]
-  root to: 'users#new'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
